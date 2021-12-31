9caa037f7fe0c151b59779fdf6d07229459558f0
Nana Mensah <Nana.mensah@crick.ac.uk>
2021-12-31 18:03:33 +0000

    Add test for read_cpg_data without cache

--- tests/testthat/test-read_cpg_data.R
+++ tests/testthat/test-read_cpg_data.R
@@ -1,3 +1,21 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("read_cpg_data without cache", {
+  
+  # Load external data
+  bed_2f <- system.file("extdata", "bismark_coverage_CpG.bedgraph", package="matt")
+  
+  # Read CpG data
+  dt <- read_cpg_data(
+    bed_2f, pipeline="bedgraph",
+    align_to_reference = F, use_cache=F,
+    collapse_strands = F
+  )
+  
+  # Assert that data.table with expected fields is returned
+  expect_true("data.table" %in% class(dt))
+  expect_true(nrow(dt) > 0)
+  expect_true(
+    all(
+      c("chr","start","cov","meth") %in% names(dt)
+    )
+  )
 })
